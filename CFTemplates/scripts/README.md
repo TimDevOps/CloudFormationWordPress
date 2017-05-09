4cbc9f31a1494e8711a82a005eb32e4bdd97433b
antoine <antoine.lagier@timspirit.fr>
2017-05-09 15:46:28 +0200

    copies des scripts retrouves dans les fichiers CF

---
+++ CFTemplates/scripts/README.md
@@ -0,0 +1,3 @@
+# Scripts
+
+here are copy of scripts used in CloudFormation files for debugging purpose
---
+++ CFTemplates/scripts/import_users.sh
@@ -0,0 +1,14 @@
+                #!/bin/bash
+                aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+                  SaveUserName="$User"
+                  SaveUserName=${SaveUserName//"+"/".plus."}
+                  SaveUserName=${SaveUserName//"="/".equal."}
+                  SaveUserName=${SaveUserName//","/".comma."}
+                  SaveUserName=${SaveUserName//"@"/".at."}
+                  if ! id -u "$SaveUserName" >/dev/null 2>&1; then
+                    #sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
+                    SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
+                    echo /usr/sbin/useradd "$SaveUserName"
+                    echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" #> "/etc/sudoers.d/$SaveUserFileName"
+                  fi
+                done
